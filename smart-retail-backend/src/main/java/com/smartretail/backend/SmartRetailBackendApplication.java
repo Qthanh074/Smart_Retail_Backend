48cfd948ef9060865be2b082bda9649936b559ab
Dao Quang Thanh <daoquangthanh0704@gmail.com>
2026-01-31 16:43:27 +0700

    Khởi tạo dự án và cấu hình application.properties

---
+++ smart-retail-backend/src/main/java/com/smartretail/backend/SmartRetailBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartretail.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRetailBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartRetailBackendApplication.class, args);
+	}
+
+}
